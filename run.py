5584f0a0d1c9175d7d048a48bfd5c22f8e699096
T-Boya <t.osewe1@gmail.com>
2018-04-05 19:35:34 +0300

    added password generation, quitting and option view options

--- run.py
+++ run.py
@@ -35,7 +35,6 @@ while True:
         break
     elif get_to_it == "n":
         print("Okay, goodbye!")
-        print("*cough* Loser *cough*")
         sys.exit()
         break
     else:
@@ -49,56 +48,70 @@ print("Search for the credentials for a specific website account (search)")
 print("Delete a website account(delete)")
 print("Generate a password (password)")
 print("And exit :( (shutdown)")
-function_choice = input("Which would you like to do?: ")
+print("Psst. You can also see you option again (options)")
 # if function_choice == display:
 
 # elif function_choice == search:
 
 # elif function_choice == delete:
-
-if function_choice == "password":
-    print("your password will be a randomly generated 7 digit number")
-    while True:
-        store_password = input("Do you want to store this passord? (y or n)?: ")
-        if store_password == "y":
-            print("To store this however, I am going to have to ask you two (simple) questions")
-            password_website = input("What website is this for?: ")
-            password_username = input("What is your username?: ")
-            password_generated_saved = str(random.randint(1000000, 10000000))
-            print("Your password is " + password_generated_saved)
-            save_user(create_user(password_website, password_username, password_generated_saved))
-            print("Your information has been saved")
-            while True:
-                continue_usage = input("Would you like to continue?: ")
-                if continue_usage == "y":
+while True:
+    function_choice = input("Which would you like to do?: ")
+    if function_choice == "password":
+        print("your password will be a randomly generated 7 digit number")
+        while True:
+            store_password = input("Do you want to store this passord? (y or n)?: ")
+            if store_password == "y":
+                print("To store this however, I am going to have to ask you two (simple) questions")
+                password_website = input("What website is this for?: ")
+                password_username = input("What is your username?: ")
+                password_generated_saved = str(random.randint(1000000, 10000000))
+                print("Your password is " + password_generated_saved)
+                save_user(create_user(password_website, password_username, password_generated_saved))
+                print("Your information has been saved")
+                while True:
+                    continue_usage = input("Would you like to continue?: ")
+                    if continue_usage == "y":
+                        break
+                    elif continue_usage == "n":
+                        print("Okay, goodbye!")
+                        sys.exit()
+                        break
+                    else:
+                        print("Please type in y or n!")
+                
+            elif store_password == "n":
+                password = random.randint(1000000, 10000000)
+                print("Your password is " + str(password))
+                while True:
+                    continue_usage = input("Would you like to continue?: ")
+                    if continue_usage == "y":
+                        break
+                    elif continue_usage == "n":
+                        print("Okay, goodbye!")
+                        sys.exit()
+                        break
+                    else:
+                        print("Please type in y or n!")
                     break
-                elif continue_usage == "n":
-                    print("Okay, goodbye!")
-                    sys.exit()
-                    break
-                else:
-                    print("Please type in y or n!")
+            else:
+                print("Please type in y or n!")
             break
-        elif store_password == "n":
-            password = random.randint(1000000, 10000000)
-            print("Your password is " + str(password))
-            while True:
-                continue_usage = input("Would you like to continue?: ")
-                if continue_usage == "y":
-                    break
-                elif continue_usage == "n":
-                    print("Okay, goodbye!")
-                    sys.exit()
-                    break
-                else:
-                    print("Please type in y or n!")
-            break
-        else:
-            print("Please type in y or n!")
 
-elif function_choice == "shutdown":
-    print("Goodbye!")
-    sys.exit()
+    elif function_choice == "shutdown":
+        print("Goodbye!")
+        sys.exit()
+
+    elif function_choice == "options":
+        print("So, here's what you can do!:")
+        print("Display all user accounts (display)")
+        print("Search for the credentials for a specific website account (search)")
+        print("Delete a website account(delete)")
+        print("Generate a password (password)")
+        print("And exit :( (shutdown)")
+
+    else:
+        print("Please enter a valid option!")
+    continue
 
 
 # # while True:
