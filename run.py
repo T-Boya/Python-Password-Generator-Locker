be4478863607d435a497d8082257b9691de9ed02
T-Boya <t.osewe1@gmail.com>
2018-04-05 16:16:02 +0300

    added first loop to continue or exit the app

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import sys
 from user import User
 
 def create_user (website, username, password):
@@ -24,5 +25,20 @@ def display_users():
     return User.display_users()
 
 greeting_name = input("What is your name: ")
-greeting_phrase = "Hello {}, Welcome to the password locker. With just a little input, I can store your passwords quickly and easily".format(greeting_name)
+greeting_phrase = "Hello {}, Welcome to the password locker. With just a little input, I can store your passwords for all sorts of different websites.".format(greeting_name)
 print(greeting_phrase)
+
+while True:
+    get_to_it = input("Shall we begin? (y or n)")
+    if get_to_it == "y":
+        print("OH YEAH!")
+        break
+    elif get_to_it == "n":
+        print("Okay, goodbye!")
+        print("*cough* Loser *cough*")
+        sys.exit()
+        break
+    else:
+        print("Please type in y or n!")
+
+print("hi")
